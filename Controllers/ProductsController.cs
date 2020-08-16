a4df17ac3f5e98a7e898d94ae0dbb9447f6fb555
Mustafa SARIEL <mustafasariel@gmail.com>
2020-08-16 22:57:43 +0300

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace redisDemo1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            this._distributedCache = distributedCache;
+           
+        }
+
+        public IActionResult Index()
+        {
+
+            DistributedCacheEntryOptions distributedCacheOption = new DistributedCacheEntryOptions();
+            distributedCacheOption.AbsoluteExpiration = DateTime.Now.AddMinutes(2);
+            _distributedCache.SetString("xyz", "100",distributedCacheOption);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.test = _distributedCache.GetString("xyz");
+            return View();
+        }
+    }
+}
